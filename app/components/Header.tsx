ea510e339b0b65499b105679bae5d90003ca2f72
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-08-11 15:49:12 +0530

    update header button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -635,7 +635,7 @@ const Header: React.FC = () => {
     className="text-gray-400 hover:text-gray-800 cursor-pointer"
   />
   <span className="text-gray-600 text-lg font-medium mx-4">EN</span>
-  <button className="bg-[#3f4249] hover:bg-[#3a3d43] text-white rounded-2xl px-4 py-4 text-lg font-medium flex items-center gap-2 transition-all">
+  <button className="bg-[#3f4249] hover:bg-[#3a3d43] text-white rounded-2xl px-5 py-4 text-lg font-medium flex items-center gap-2 transition-all">
   Get started <ArrowRight size={24} />
 </button>
 </div>
